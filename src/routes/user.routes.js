caf593d40b1d8bb1981f712fb2f5689caf9f2058
NightRiderUltraMax <106510732+nightridermax@users.noreply.github.com>
2024-05-18 14:35:55 +0530

    bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import {Router} from 'express';
 import {registerUser} from '../controllers/user.controllers.js';
-
+import { upload } from '../middleware/multter.middleware.js';
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 3
+        }
+    ]),
+    registerUser
+)
 
 export default router 
--- src/utils/cloudinay.js
+++ src/utils/cloudinay.js
@@ -6,3 +6,23 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret:  process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFileStorage) => {
+  try {
+    if (!localFileStorage) return null
+    //upload on cloudinary
+    const response = cloudinary.uploader.upload
+    (localFileStorage, {
+      resource_type: "auto"
+    })
+    //file has been upload succefully
+    console.log("file is uploaded on cloudinary", (await response).url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFileStorage)
+    return null;
+  }
+}
+
+
+export {uploadOnCloudinary}
