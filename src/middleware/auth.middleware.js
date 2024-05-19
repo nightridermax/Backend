f3e5b7f2bd417eea710c263f8ff3ceb2297c1622
NightRiderUltraMax <106510732+nightridermax@users.noreply.github.com>
2024-05-19 18:09:55 +0530

    auth middleware added

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import { ApiError } from "../utils/ApiErrors";
+import { asyncHandler } from "../utils/asyncHandlers.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+export const verifyJWT = asyncHandler(async(req, res, next) =>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if (!token) {
+            throw new ApiError(401, "unauthorized user")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        if (!decodedToken) {
+            throw new ApiError(401, "kal ache se shikh ke aana beta: haking")
+        }
+    
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+    
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(500, "verifyJWT failed")
+    }
+})
+
